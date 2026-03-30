0393a07dda7b35d5871927d51fccbb1abe842186
webdevoliva123 <x0abhie@gmail.com>
2026-03-30 18:33:30 +0530

    feat: initialize backend structure with Express and MongoDB integration
    - Set up Express server in index.ts with database connection handling.
    - Create app.ts to define API routes and middleware.
    - Implement database connection logic in database.config.ts using Mongoose.
    - Define Mongoose models for Chat, Message, and User with appropriate schemas.
    - Establish route files for authentication, user, message, and chat functionalities.
    - Update package.json to include necessary dependencies and scripts for development and production.

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import app from "./src/app";
+import { CONNECT_DB } from "./src/config/database.config";
+
+CONNECT_DB().then(() => {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Server is running on port : ${PORT} 🚀`);
+    });
+}).catch((error) => {
+    console.error("Failed to connect to the database. Server not started.", error);
+});
---
+++ backend/src/config/database.config.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+export const CONNECT_DB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI!);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        process.exit(1); // exit with failure
+
+        //Status code 1 means failure, 0 means success. 
+        //By convention, a non-zero exit code indicates that an error occurred. 
+        // In this case, if the connection to MongoDB fails, we log the error and exit the process with a status code of 1 to indicate that there was a failure in connecting to the database.
+    }
+}
