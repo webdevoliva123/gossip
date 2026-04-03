6a7901426a5941bb630e971738be9eadcbc06768
webdevoliva123 <x0abhie@gmail.com>
2026-04-03 15:18:10 +0530

    refactor: simplify authentication error handling and improve loading state management

--- mobile/hooks/useAuthSocial.ts
+++ mobile/hooks/useAuthSocial.ts
@@ -1,38 +1,28 @@
 import { useSSO } from "@clerk/expo";
 import { useState } from "react";
+import { Alert } from "react-native";
 
 const useAuthSocial = () => {
     const [loadingStrategy, setLoadingStrategy] = useState<"oauth_google" | "oauth_apple" | null>(null);
-    const [errorStrategy, setErrorStrategy] = useState<{
-        strategy: "oauth_google" | "oauth_apple",
-        message: string
-    } | null>(null);
     const { startSSOFlow } = useSSO();
 
     const handleSocialAuth = async (strategy: "oauth_google" | "oauth_apple") => {
+        if (loadingStrategy) return
         setLoadingStrategy(strategy);
-        setErrorStrategy(null)
         try {
             const { createdSessionId, setActive } = await startSSOFlow({ strategy });
 
-
-            if (createdSessionId && setActive) {
-                await setActive({ session: createdSessionId });
-            } else {
-                setErrorStrategy({
-                    strategy,
-                    message: "Failed to authenticate with the selected provider. Please try again."
-                });
+            if (!createdSessionId || !setActive) {
                 setLoadingStrategy(null);
+                Alert.alert("Authentication Error", "An error occurred during social authentication. Please try again.")
+                return
             }
+
+            await setActive({ session: createdSessionId });
+
         } catch (error) {
             console.log({ error });
-
-            setErrorStrategy({
-                strategy,
-                message: "An error occurred during social authentication. Please try again."
-            });
-
+            Alert.alert("Authentication Error", "An error occurred during social authentication. Please try again.")
         } finally {
             setLoadingStrategy(null);
         }
@@ -40,7 +30,7 @@ const useAuthSocial = () => {
 
     }
 
-    return { handleSocialAuth, loadingStrategy, errorStrategy }
+    return { handleSocialAuth, loadingStrategy }
 }
 
 
